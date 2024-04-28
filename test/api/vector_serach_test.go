f049b15fcbbfd35c7d076687b0f9e7737337b70f
Kaniu <kaniucc@qq.com>
2024-04-28 14:55:54 +0800

    Modify the accuracy of flat search tests (#2)
    Co-authored-by: kaniu <bitao.shi@seafile.com>

--- test/api/vector_serach_test.go
+++ test/api/vector_serach_test.go
@@ -42,7 +42,10 @@ var documents = `{ "index" : {"_index" : "paper" } }
 { "index" : {"_index" : "paper" } } 
 {"paper-id": "002","title-vec":[10.2,11.40,9.5,22.2]}
 { "index" : {"_index" : "paper" } } 
-{"paper-id": "003","title-vec":[10.2,12.40,9.5,22.2]}`
+{"paper-id": "003","title-vec":[10.2,12.40,9.5,22.2]}
+{ "index" : {"_index" : "paper" } }
+{"paper-id": "004","title-vec":[10.2,10.39,9.5,22.2]}
+`
 
 var searchParam = `
 {
@@ -50,9 +53,74 @@ var searchParam = `
     "k":3,
     "return_fields":["paper-id"],
     "vector":[10.2,10.39,9.5,22.2],
-    "_source": false
+    "_source": ["paper-id"]
 }
 `
+
+// result by faiss index
+var vectorSearchResult = `
+{
+	"took": 0,
+	"timed_out": false,
+	"_shards": {
+		"total": 1,
+		"successful": 1,
+		"skipped": 0,
+		"failed": 0
+	},
+	"hits": {
+		"total": {
+			"value": 3
+		},
+		"max_score": 1,
+		"hits": [
+			{
+				"_index": "paper",
+				"_type": "_doc",
+				"_id": "27OC23UOaQP",
+				"_score": 1,
+				"_source": {
+					"paper-id": "004"
+				},
+				"fields": {
+					"paper-id": [
+						"004"
+					]
+				}
+			},
+			{
+				"_index": "paper",
+				"_type": "_doc",
+				"_id": "27OC23UOaQM",
+				"_score": 0.9998999834060669,
+				"_source": {
+					"paper-id": "001"
+				},
+				"fields": {
+					"paper-id": [
+						"001"
+					]
+				}
+			},
+			{
+				"_index": "paper",
+				"_type": "_doc",
+				"_id": "27OC23UOaQN",
+				"_score": 0.49502530694007874,
+				"_source": {
+					"paper-id": "002"
+				},
+				"fields": {
+					"paper-id": [
+						"002"
+					]
+				}
+			}
+		]
+	}
+}
+`
+
 var deleteByQuery = `
 {
     "query": {
@@ -122,7 +190,16 @@ func TestVectorSearch(t *testing.T) {
 		var result = &meta.SearchResponse{}
 		b := resp.Body.Bytes()
 		_ = json.Unmarshal(b, &result)
-		assert.Equal(t, 3, result.Hits.Total.Value)
+		var vecResult = &meta.SearchResponse{}
+		_ = json.Unmarshal([]byte(vectorSearchResult), &vecResult)
+		assert.Equal(t, vecResult.Hits.Total.Value, result.Hits.Total.Value)
+		assert.Equal(t, vecResult.Hits.MaxScore, result.Hits.MaxScore)
+		for i := 0; i < len(vecResult.Hits.Hits); i++ {
+			assert.Equal(t, vecResult.Hits.Hits[i].Source, result.Hits.Hits[i].Source) // paper-id
+			assert.Equal(t, vecResult.Hits.Hits[i].Fields, result.Hits.Hits[i].Fields) // paper-id
+			assert.Equal(t, vecResult.Hits.Hits[i].Score, result.Hits.Hits[i].Score)
+		}
+
 	})
 
 	t.Run("delete by query", func(t *testing.T) {
@@ -140,7 +217,18 @@ func TestVectorSearch(t *testing.T) {
 		var result = &meta.SearchResponse{}
 		b := resp.Body.Bytes()
 		_ = json.Unmarshal(b, &result)
-		assert.Equal(t, 2, result.Hits.Total.Value)
+		assert.Equal(t, 3, result.Hits.Total.Value)
+
+		var vecResult = &meta.SearchResponse{}
+		// after delete 003
+		_ = json.Unmarshal([]byte(vectorSearchResult), &vecResult)
+		assert.Equal(t, vecResult.Hits.Total.Value, result.Hits.Total.Value)
+		assert.Equal(t, vecResult.Hits.MaxScore, result.Hits.MaxScore)
+		for i := 0; i < len(vecResult.Hits.Hits); i++ {
+			assert.Equal(t, vecResult.Hits.Hits[i].Source, result.Hits.Hits[i].Source) // paper-id
+			assert.Equal(t, vecResult.Hits.Hits[i].Fields, result.Hits.Hits[i].Fields) // paper-id
+			assert.Equal(t, vecResult.Hits.Hits[i].Score, result.Hits.Hits[i].Score)
+		}
 	})
 
 	t.Run("delete Index", func(t *testing.T) {
