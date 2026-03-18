cd83db6fe95ffd11de826d40e34ea97d1d52ee39
rumtid <liwei.jiang@seafile.com>
2026-03-18 18:48:32 +0800

    Remove web UI and related components (#56)

---
+++ main.go
@@ -0,0 +1,5 @@
+package zincsearch
+
+// This file exists solely as a placeholder to ensure that the Go tool
+// recognizes this directory when using patterns like "./..."
+// (e.g., "go test ./...").
